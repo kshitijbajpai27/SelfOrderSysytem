2587e52af1afbb1addf5af65456efd8839e4bfc9
Shubham Kanaujiya <84391771+kshitijbajpai27@users.noreply.github.com>
2021-12-15 15:42:32 +0530

    SelfOrderSystem

---
+++ customer/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='menu_images/')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ManyToManyField('Category', related_name='item')
+    quantity = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return f"{self.name}, {self.description}, {self.image}, {self.price}, {self.category}, {self.quantity}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class OrderModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
+    name = models.CharField(max_length=50, blank=True)
+    phone = models.IntegerField(blank=True, null=True)
+    #option = models.CharField(max_length=10)
+    is_paid = models.BooleanField(default=False)
+    is_shipped = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
